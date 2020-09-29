c70e417af73c96168366d8684a386dea6ff30291
rubangarmon <rubangarmon99@gmail.com>
2020-09-28 23:19:06 -0600

    ALL

---
+++ Shared.Models/Models/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservices.Ticketing.WebApi.Models
+{
+    public class Ticket
+    {
+        public string UserName { get; set; }
+        public DateTime BookedOn { get; set; }
+        public string Boarding { get; set; }
+        public string Destination { get; set; }
+    }
+}
---
+++ Ticketing.Microservice/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microservices.Ticketing.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ticketing.Microservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class TicketController : ControllerBase
+    {
+        private readonly IBus _bus;
+        public TicketController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(Ticket ticket)
+        {
+            if (ticket != null)
+            {
+                ticket.BookedOn = DateTime.Now;
+                Uri uri = new Uri("---");
+                var endopoint = await _bus.GetSendEndpoint(uri);
+                await endopoint.Send(ticket);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
+        [HttpGet]
+        public async Task<string> Meme()
+        {
+            return "it's me Mario";
+        }
+    }
+}
